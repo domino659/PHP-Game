f955f651753856b999fb2e6cf3de382563f2fe3f
Martin SION <martin@sionfamily.com>
2021-10-30 12:13:07 +0200

    touche sur le front et readme

--- src/index.php
+++ src/index.php
@@ -6,12 +6,8 @@ require $className . '.php';
 $request = new HumanManager();
 $humans = $request->getAll();
 // var_dump($humans);
-// var_dump($humans[0]['class']);
 //$request->getOne(3);
-//$request->addHuman('Zeremy', 100, 20, 10, 'Zerem', false);
-//$request->addHuman('Sammy', 10, 2, 1, 'Sammy', true);
 //$request->removeHuman(23)
-//var_dump($human);
 
 ?>
 <!DOCTYPE html>
@@ -19,24 +15,29 @@ $humans = $request->getAll();
   <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <link rel="stylesheet" href="css/index.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>GamePhp</title>
   </head>
   <body>
-    <pre id="human_list">
+    <div id="human_list">
     <?php
         foreach($humans as $human) {
-            // var_dump($human);
             $id = $human->getId();
-            print($human->getName());
-            print($human->getpv());
-            print($human->getAttack());
-            print($human->getDefence());
             ?>
+            <p>
+                <?php
+                print($human->getName());
+                print($human->getpv());
+                print($human->getAttack());
+                print($human->getDefence());
+                print(get_class($human));
+                ?>
+            </p>
             <button class="select" id=<?php echo $id; ?>>Select</button>
             <br><?php
         } ?>
-    </pre>
+    </div>
     <div class="container fill">
         <form action="index.php">
             <label>Create your hero</label>
@@ -48,7 +49,7 @@ $humans = $request->getAll();
                 <option value="Tank">Tank</option>
                 <option value="Magician">Magician</option>
             </select>
-            <button id="human_form" type="submit" value="Create">Create</button>
+            <button id="human_form" class="select" type="submit" value="Create">Create</button>
         </form>
     </div>
   </body>
@@ -56,9 +57,7 @@ $humans = $request->getAll();
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script>
-
     //Ajout personnage dans base de données
-
     human_form = document.querySelector("#human_form");
     human_form.addEventListener("click", function(human_form){
 
@@ -87,16 +86,24 @@ $humans = $request->getAll();
                     human_defense: human_defense,
                     human_atk: human_atk,
                 },
-                success: function(data) { 
+                success: function(data) {
                     human_list = document.querySelector('#human_list')
-                    char = document.createElement("pre")
+                    char = document.createElement("p")
+                    button = document.createElement("button")
+                    button.id = "human_form"
+                    button.class = "select"
+                    button.type = "submit"
+                    button.Create = "Create"
+                    button.innerHTML = "Select"
                     switch(human_class){
                         case "Warrior" : char.textContent = human_name+" "+human_class+" "+human_pv+" "+human_atk+" "+human_defense; break;
                         case "Archer" : char.textContent = human_name+" "+human_class+" "+human_pv+" "+human_atk+" "+human_defense; break;
                         case "Tank" : char.textContent = human_name+" "+human_class+" "+human_pv+" "+human_atk+" "+human_defense; break;
                         case "Magician" : char.textContent = human_name+" "+human_class+" "+human_pv+" "+human_atk+" "+human_defense; break;
                     }
+
                     human_list.appendChild(char)
+                    human_list.appendChild(button)
                 }
             });
         }
@@ -104,14 +111,11 @@ $humans = $request->getAll();
 </script>
 
 <script>
-
     //Gestion bouton choix personnage bagarre
-
     all_button_select = document.getElementsByClassName("select")
     // all_button_select.foreach{
     //     button.addEventListener("click", function(){
     //         button.class = "selected"
     //     })
-    }
-
+    // }
 </script>
